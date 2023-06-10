c817dc23f0610370347e6aabf30ec59aa9090a33
josecardoza22 <000898@uca.edu.sv>
2023-06-10 08:18:13 -0600

    Capa Modelo

---
+++ app/src/androidTest/java/com/enrique/parcialmoviles/data/DummyData.kt
@@ -0,0 +1,15 @@
+package com.enrique.parcialmoviles.data
+
+import com.enrique.parcialmoviles.data.models.PlantModel
+
+val type = "Rosas"
+val color = "Rojo"
+
+val type2 = "Girasoles"
+val color2 = "Amarillo"
+
+val plants = mutableListOf(
+    PlantModel(type, color),
+    PlantModel(type2, color2)
+)
+
---
+++ app/src/androidTest/java/com/enrique/parcialmoviles/data/models/PlantModel.kt
@@ -0,0 +1,6 @@
+package com.enrique.parcialmoviles.data.models
+
+class PlantModel(
+    val type: String,
+    val color: String
+)
